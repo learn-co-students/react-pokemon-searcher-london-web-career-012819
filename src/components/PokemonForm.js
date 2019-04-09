edb7e045eb58ee36ca8ae56802fb10f9a52401b0
Alexander Dubois <sander.dubois@outlook.com>
2019-04-09 17:26:43 +0100

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,24 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={() => this.props.handleFormSubmit(this.state)}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.handleChange}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp"  onChange={this.handleChange}/>
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl"  onChange={this.handleChange} />
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl"  onChange={this.handleChange} />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
