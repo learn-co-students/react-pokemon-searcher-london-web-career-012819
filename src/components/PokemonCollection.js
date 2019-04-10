1c44929862f64988a0cc22af9d38f7bd869adbf2
Kian Edalat <akedalat@gmail.com>
2019-04-10 13:07:28 +0100

    search functionality tested

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,14 +2,38 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
+
+
 class PokemonCollection extends React.Component {
+
+  state = {
+    pokemon: []
+  }   
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pokemon")
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        pokemon: data  
+      }) 
+    }) 
+  }
+
+  matchSearch = () => {
+    return this.state.pokemon.filter(pokemon => pokemon.name.includes(this.props.searchTerm))
+  }
+  
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
+      {this.matchSearch().map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon} /> )}
         <h1>Hello From Pokemon Collection</h1>
       </Card.Group>
     )
   }
 }
+{/* <PokemonCard pokemon={this.state.pokemon[0]}/> */}
 
 export default PokemonCollection
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,24 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state= {
+    searchTerm: ""
+  }
+
+  handleSearch = (e, { value }) => {
+    this.setState({ searchTerm: value })
+  }
+  
   render() {
+    console.log(this.state.searchTerm)
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.handleSearch)} value={this.state.searchTerm} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection searchTerm={this.state.searchTerm} />
         <br />
         <PokemonForm />
       </div>
